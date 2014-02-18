d16dafe1b86691319f91822cdd962bf669d1c82b
easauceda <easauceda@gmail.com>
2014-02-17 16:47:18 -0800

    Made some minor changes -fleshed out the inputTest a bit more, details in the file

--- SheepDieGameRevision2/src/inputTest.java
+++ SheepDieGameRevision2/src/inputTest.java
@@ -3,16 +3,42 @@ import java.io.FileNotFoundException;
 import java.util.Scanner;
 
 //Idea of this test file is to implement a file reader for the homework
+/*
+ * what the file does right now is it reads the text file word by word and checks 
+ * for keywords such as wolf or sheep in the compareForNextStep method.
+ */
 public class inputTest {
 
 	public static void main(String[] args) throws FileNotFoundException {
 		Scanner fileReader = new Scanner(new File("input.txt"));
-		while(fileReader.hasNext()){
+		while (fileReader.hasNext()) {
 			String data = fileReader.next();
-			System.out.println(data);
+			//send data to check for keyword
+			int answer = compareForNextStep(data);
+			//Right now all it does is print out the answer we get from compareForNextStep
+			System.out.println(answer);
 		}
+
 		fileReader.close();
 
 	}
 
+	public static int compareForNextStep(String data) {
+		//if there's a hit on a keyword, the int being returned will change
+		if (data.equalsIgnoreCase("clock")) {
+			return 1;
+		}
+		if (data.equalsIgnoreCase("sheep")) {
+			return 2;
+		}
+		if (data.equalsIgnoreCase("wolf")) {
+			return 3;
+		} else {
+			return 0;
+		}
+	}
+/*
+ * the idea is to pull that number and use it to tell the program what to do next, create a wolf
+ * create sheep, or change the time clock.
+ */
 }
