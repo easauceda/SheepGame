fbb3f871ced728c951867fa6a55128fc51082dae
easauceda <easauceda@gmail.com>
2014-02-17 16:37:01 -0800

    Commit and push edits on inputTest

--- SheepDieGameRevision2/src/inputTest.java
+++ SheepDieGameRevision2/src/inputTest.java
@@ -7,7 +7,6 @@ public class inputTest {
 
 	public static void main(String[] args) throws FileNotFoundException {
 		Scanner fileReader = new Scanner(new File("input.txt"));
-		//must get this scanner to read text
 		while(fileReader.hasNext()){
 			String data = fileReader.next();
 			System.out.println(data);
