4771a3bb951cc52b1d54779e97894aa1e9752352
easauceda <easauceda@gmail.com>
2014-02-17 16:25:18 -0800

    removed the test comment and the code that commented out one of the sheep

--- SheepDieGameRevision2/src/Test.java
+++ SheepDieGameRevision2/src/Test.java
@@ -1,14 +1,13 @@
 import java.awt.Color;
 import java.util.Collection;
 import java.util.PriorityQueue;
-//test
 public class Test {
 
 	public static void main(String[] args) {
 		PriorityQueue<Sheep> sheeps = new PriorityQueue<Sheep>();
 
 		sheeps.add(new Sheep(10, 10, Color.black));
-		//sheeps.add(new Sheep(2, 2, Color.black));
+		sheeps.add(new Sheep(2, 2, Color.black));
 		sheeps.add(new Sheep(4, 4, Color.black));
 		sheeps.add(new Sheep(7, 7, Color.black));
 		sheeps.add(new Sheep(3, 3, Color.black));
