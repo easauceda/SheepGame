19c1ede930eaf5cb509935ca998796b385f1bf08
easauceda <easauceda@gmail.com>
2014-02-24 00:14:25 -0800

    d

--- SheepDieGameRevision2/src/GameSettings.java
+++ SheepDieGameRevision2/src/GameSettings.java
@@ -1,30 +1,27 @@
 
-
 import java.awt.Color;
-
 
 public interface GameSettings {
 	int B_WIDTH = 400;
 	int B_HEIGHT = 400;
 	int X_MARGIN = 20;
 	int Y_MARGIN = 20;
-	
+
 	boolean debugMode = true;
 	int deathCountValue = 10;
-	
+
 	int windowSizeY = 500;
 	int windowSizeX = 400;
-	
-	int numberOfWolfs = 5;
-	int numberOfSheeps = 20;
-	
+
+	int numberOfWolfs = input.getNumberOfWolfs();
+	int numberOfSheeps = input.getNumberOfSheep();
+
 	int max_X = 20;
 	int max_Y = 20;
-	
-	
-	int GAME_SPEED = 400;
+
+	int GAME_SPEED = input.getSpeed();
 	int BOARD_REFRESH_RATE = 50;
-	
-	Color SHEEP_COLOR = new Color(255,255,255);
+
+	Color SHEEP_COLOR = new Color(255, 255, 255);
 	Color WOLF_COLOR = Color.BLACK;
 }
