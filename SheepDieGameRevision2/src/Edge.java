49fea9efed1c917a31b55827001a9a03c452f7a9
easauceda <easauceda@gmail.com>
2014-03-11 22:47:55 -0700

    Cleaned up a bit

--- SheepDieGameRevision2/src/Edge.java
+++ SheepDieGameRevision2/src/Edge.java
@@ -10,8 +10,7 @@ public class Edge implements GameSettings {
 	private double weight;
 	protected int ystep = B_HEIGHT / 20;
 	protected int xstep = B_WIDTH / 20;
-	
-	
+
 	public Edge(Node lead, Node end) {
 		setLead(lead);
 		setEnd(end);
@@ -26,36 +25,29 @@ public class Edge implements GameSettings {
 	private void setEnd(Node end) {
 		this.end = end;
 	}
-	public Node getLead(){
+
+	public Node getLead() {
 		return lead;
-		
+
 	}
-	public Node getEnd(){
+
+	public Node getEnd() {
 		return end;
-		
+
 	}
-	private void calcWeight() {
-		//used distance formula to calculate weight
-		int leadX = lead.getX();
-		int leadY = lead.getY();
-		int endX = end.getX();
-		int endY = end.getY();
-		double finalX = Math.pow((leadX - endX),2);
-		double finalY = Math.pow((leadY - endY),2);
-		double addedVal = (finalX + finalY);
-		weight = Math.pow(addedVal, .5);
-	}
-@Override
-	public String toString(){
-	System.out.println("Weight: " + weight);
+
+	@Override
+	public String toString() {
+		System.out.println("Weight: " + weight);
 		return "----END----";
 	}
 
 	void paint(Graphics pen) {
-		
-	pen.setColor(Color.green);
-	pen.drawLine(X_MARGIN + xstep * end.getX() + 10, Y_MARGIN + ystep
-			* end.getY() + 10, X_MARGIN + xstep * lead.getX() + 10,
-			Y_MARGIN + ystep * lead.getY() + 10);
-}
+
+		pen.setColor(Color.black);
+		pen.drawLine(X_MARGIN + xstep * end.getX() + 10,
+				Y_MARGIN + ystep * end.getY() + 10,
+				X_MARGIN + xstep * lead.getX() + 10,
+				Y_MARGIN + ystep * lead.getY() + 10);
+	}
 }
